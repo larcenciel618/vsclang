278543e5ceaa0e042f22fa1e61236dbbb567f174
larcenciel618 <hhiirroossee816@gmail.com>
2021-01-26 04:08:34 +0900

    launch,tasksedit

--- clang.c
+++ clang.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
 
-int main(void)
+/*int main(void)
 {
     printf("hello gitworld\n");
     return 0;
-}
+}*/
 
+int main(void)
+{
+    int i ,high=0 ;
+    long org=600851475143;
+    
+    for(i = 2;i <= org;i ++){
+        if(org % i == 0){
+            do{
+                org = org/i;
+                high = i;
+            }while(org%i == 0);
+        }
+    }
+    printf("%d\n",high);
+    return 0;
+}
